ef0680b89784ec863f91749bc3c8404cdec4d601
Jerry Miao <jm2284@cornell.edu>
2023-02-03 09:08:46 -0700

    added backend project

---
+++ shred-backend/src/main/java/net/shredapp/shredbackend/ShredBackendApplication.java
@@ -0,0 +1,13 @@
+package net.shredapp.shredbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShredBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShredBackendApplication.class, args);
+	}
+
+}
---
+++ shred-backend/src/test/java/net/shredapp/shredbackend/ShredBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.shredapp.shredbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShredBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
