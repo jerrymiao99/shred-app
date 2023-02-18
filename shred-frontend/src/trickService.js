0e4381c67810ede17de88b3ef571fb01002cab0f
jerrymiao99 <jerrymiao99@gmail.com>
2023-02-17 18:14:12 -0700

    server port

--- shred-frontend/src/trickService.js
+++ shred-frontend/src/trickService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const REST_API_URL = 'http://localhost:8080';
+const REST_API_URL = 'http://localhost:5000';
 
 export const getNodesEdges = () => {
   return axios.get(REST_API_URL + '/');
